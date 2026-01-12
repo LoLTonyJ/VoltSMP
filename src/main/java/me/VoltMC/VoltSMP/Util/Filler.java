312499fdd775ade29dbb30bf0fdbf58d2fda3ed2
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-12 16:03:03 -0500

    Fixed Display error.

--- src/main/java/me/VoltMC/VoltSMP/Util/Filler.java
+++ src/main/java/me/VoltMC/VoltSMP/Util/Filler.java
@@ -9,7 +9,7 @@ public class Filler {
     public static ItemStack redFiller() {
         ItemStack item = new ItemStack(Material.RED_STAINED_GLASS_PANE);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(" ");
+        meta.setDisplayName(ChatColor.format("&c "));
         meta.getItemFlags().clear();
         item.setItemMeta(meta);
         return item;
