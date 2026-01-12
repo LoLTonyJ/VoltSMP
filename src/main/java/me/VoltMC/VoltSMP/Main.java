0adeb08486b6943d52d260d7a13ea1d231174970
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-12 16:03:17 -0500

    Initialized Playtime Data Files, and Load Funcs.

--- src/main/java/me/VoltMC/VoltSMP/Main.java
+++ src/main/java/me/VoltMC/VoltSMP/Main.java
@@ -1,8 +1,11 @@
 package me.VoltMC.VoltSMP;
 
 import me.VoltMC.VoltSMP.Commands.RankGUI;
+import me.VoltMC.VoltSMP.FileManip.PlaytimeData;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
 import java.util.logging.Level;
 
 public final class Main extends JavaPlugin {
@@ -11,15 +14,22 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        String version = this.getConfig().getString("Plugin Version");
+        String version = this.getConfig().getString("version");
         instance = this;
 
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
+        // File Loads.
+        try {
+            PlaytimeData.Load();
+        } catch (IOException | InvalidConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+
         getCommand("testgui").setExecutor(new RankGUI());
 
-        this.getLogger().log(Level.INFO, "VOLTSMP Main Plugin has been started. " + version);
+        this.getLogger().log(Level.INFO, "VoltSMP Main Plugin has been started. " + version);
         this.getLogger().log(Level.INFO, " ");
         this.getLogger().log(Level.INFO, "If there is anything wrong with the loading param please contact Ghostinq on Discord.");
 
