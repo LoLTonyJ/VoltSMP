f9ee90326a36961e58485b247c6405f8094c6f89
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-13 23:13:36 -0500

    Added Load File logic for Gamer, Miner, and Traveler. Saved Data files on Server Close / Plugin Reload. Implemented Luckperms API.

--- src/main/java/me/VoltMC/VoltSMP/Main.java
+++ src/main/java/me/VoltMC/VoltSMP/Main.java
@@ -1,10 +1,19 @@
 package me.VoltMC.VoltSMP;
 
+import me.VoltMC.VoltSMP.Commands.AdminCommands;
 import me.VoltMC.VoltSMP.Commands.Playtime;
 import me.VoltMC.VoltSMP.Commands.RankGUI;
 import me.VoltMC.VoltSMP.FileManip.PlaytimeData;
+import me.VoltMC.VoltSMP.FileManip.RankTracking.MinerTrack;
+import me.VoltMC.VoltSMP.FileManip.RankTracking.TravelerTrack;
 import me.VoltMC.VoltSMP.Functions.playtimeEvents;
+import me.VoltMC.VoltSMP.Ranks.GoalEvents.Miner;
+import me.VoltMC.VoltSMP.Ranks.GoalEvents.Traveler;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -12,30 +21,30 @@ import java.util.logging.Level;
 
 public final class Main extends JavaPlugin {
 
-    static Main instance;
+    private static Main instance;
+    private LuckPerms luckPerms;
 
     @Override
     public void onEnable() {
         String version = this.getConfig().getString("version");
         instance = this;
 
+        // LuckPerms Registry
+        luckPerms = LuckPermsProvider.get();
+
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
         // Events.
         getServer().getPluginManager().registerEvents(new playtimeEvents(), this);
-
-
-        // Player Data File Load.
-        PlaytimeData.loadPlayerTimes();
-
-        this.getLogger().log(Level.INFO, "Player Data has loaded successfully.");
+        getServer().getPluginManager().registerEvents(new Traveler(), this );
+        getServer().getPluginManager().registerEvents(new Miner(), this);
 
         // File Loads.
         try {
             PlaytimeData.Load();
-
-
+            TravelerTrack.Load();
+            MinerTrack.Load();
             this.getLogger().log(Level.INFO, "File Data has loaded successfully.");
         } catch (IOException | InvalidConfigurationException e) {
             throw new RuntimeException(e);
@@ -43,6 +52,12 @@ public final class Main extends JavaPlugin {
 
         getCommand("testgui").setExecutor(new RankGUI());
         getCommand("playtime").setExecutor(new Playtime());
+        getCommand("voltadmin").setExecutor(new AdminCommands());
+
+
+        // Player Data File Load.
+        PlaytimeData.loadPlayerTimes();
+        this.getLogger().log(Level.INFO, "Player Data has loaded successfully.");
 
         this.getLogger().log(Level.INFO, "VoltSMP Main Plugin has been started. " + version);
         this.getLogger().log(Level.INFO, " ");
@@ -52,6 +67,14 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        PlaytimeData.Save();
+        TravelerTrack.Save();
+        MinerTrack.Save();
+    }
+
+
+    public LuckPerms luckAPI() {
+        return luckPerms;
     }
 
     public String getPrefix() {
