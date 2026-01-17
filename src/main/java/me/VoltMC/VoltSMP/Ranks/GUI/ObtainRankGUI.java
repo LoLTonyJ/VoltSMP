5465dc2e657a3f3802cc04a587d60bb90408a20c
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-17 17:26:23 -0500

    Added Filler Nums, and Rank GUI Items

--- src/main/java/me/VoltMC/VoltSMP/Ranks/GUI/ObtainRankGUI.java
+++ src/main/java/me/VoltMC/VoltSMP/Ranks/GUI/ObtainRankGUI.java
@@ -1,5 +1,6 @@
 package me.VoltMC.VoltSMP.Ranks.GUI;
 
+import me.VoltMC.VoltSMP.Ranks.RankItems.Miner;
 import me.VoltMC.VoltSMP.Ranks.RankItems.Traveler;
 import me.VoltMC.VoltSMP.Util.Filler;
 import org.bukkit.Bukkit;
@@ -19,14 +20,15 @@ public class ObtainRankGUI {
         Inventory inv = Bukkit.createInventory(null, 9*6, ChatColor.AQUA + "Obtainable Ranks"); // bing bong.
 
         // Filler Item
-        int[] filler = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // Blah blah numbers, numbers.
+        int[] filler = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 17, 18, 26, 27, 35, 36, 44, 45, 46, 47 ,48, 49, 50, 51, 52, 53}; // Blah blah numbers, numbers.
         for (int i = 0; i <= inv.getSize(); i++){ // Loops through Empty Inventory Slots.
             for (int f : filler) { // Checks if f == int from filler[] (Line 21)
                 if (i == f) inv.setItem(f, Filler.redFiller()); // If f == filler[] inputs filler item.
             }
         }
 
-        inv.setItem(21, Traveler.travelItem(p));
+        inv.setItem(11, Traveler.travelItem(p));
+        inv.setItem(12, Miner.minerItem(p));
 
         p.openInventory(inv);
         return inv;
