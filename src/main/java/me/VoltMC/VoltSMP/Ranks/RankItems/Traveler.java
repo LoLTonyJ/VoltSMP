4fb1655628d271e87386cd05133dc7a083e379b4
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-17 17:29:28 -0500

    Added Seperate Lores depending on players goal.

--- src/main/java/me/VoltMC/VoltSMP/Ranks/RankItems/Traveler.java
+++ src/main/java/me/VoltMC/VoltSMP/Ranks/RankItems/Traveler.java
@@ -1,5 +1,6 @@
 package me.VoltMC.VoltSMP.Ranks.RankItems;
 
+import me.VoltMC.VoltSMP.FileManip.RankTracking.TravelerTrack;
 import me.VoltMC.VoltSMP.Main;
 import me.VoltMC.VoltSMP.Util.ChatColor;
 import org.bukkit.Material;
@@ -17,14 +18,24 @@ public class Traveler {
         // Param @p will be used once config files are running smoothly. Until then, it is a placeholder for future inputs.
 
         String blocks = Main.getInstance().getConfig().getString("Traveler.Blocks");
+        Integer blockGoal = Integer.parseInt(Main.getInstance().getConfig().getString("Traveler.Blocks"));
 
         ItemStack item = new ItemStack(Material.COMPASS);
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(ChatColor.format("&bTraveler Rank"));
         List<String> lore = new ArrayList<>();
+        lore.add(" ");
         lore.add(ChatColor.format("&bObtainable by;"));
         lore.add(ChatColor.format("&7- &aWalking " + blocks + " blocks"));
-        lore.add(" ");
+        if (TravelerTrack.getPlayerBlock(p) <= blockGoal) {
+            lore.add(" ");
+            lore.add(ChatColor.format("&aYou have traveled " + TravelerTrack.getPlayerBlock(p) + " Blocks"));
+            lore.add(" ");
+        } else {
+            lore.add(" ");
+            lore.add(ChatColor.format("&a&lClick to claim your Reward!"));
+            lore.add(" ");
+        }
         meta.setLore(lore);
         item.setItemMeta(meta);
         return item;
