85f97b432ae716327e0b07e300488b7a0458f7bf
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-19 16:47:33 -0500

    Implemented Methods for Rewards.

--- src/main/java/me/VoltMC/VoltSMP/FileManip/RankRewards/MinerRewards.java
+++ src/main/java/me/VoltMC/VoltSMP/FileManip/RankRewards/MinerRewards.java
@@ -1,18 +1,41 @@
 package me.VoltMC.VoltSMP.FileManip.RankRewards;
 
 import me.VoltMC.VoltSMP.Main;
+import me.VoltMC.VoltSMP.Util.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 public class MinerRewards {
 
     private static File file;
     private static YamlConfiguration config;
+
+    public static void giveMoney(Player p, Integer amount) {
+        Main.getEconomy().bankDeposit(p.getName(), amount);
+        p.sendMessage(ChatColor.format("&7You have been given &a$" + amount));
+    }
+
+    public static Integer getRewardAmount(String path) {
+        if (path == null) return 0;
+        return config.getInt(path);
+    }
+
+    public static List<String> getList(Integer Tier) {
+        ConfigurationSection rewardSection = config.getConfigurationSection("miner_tier_" + Tier);
+        if (rewardSection == null) {
+            Main.getInstance().getLogger().log(Level.SEVERE, "Reward List Came back as NULL! (Miner)");
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(rewardSection.getKeys(false));
+    }
 
     public static void Save() {
         file = new File(Main.getInstance().getDataFolder(), "RankProgress/RankRewards/MinerRewards.yml");
