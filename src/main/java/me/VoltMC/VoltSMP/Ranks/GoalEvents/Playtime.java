83d484379cdfaebfb8c5a2578d8cfa94ee6dd48a
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-19 16:48:08 -0500

    Implemented Playtime Tracking.

---
+++ src/main/java/me/VoltMC/VoltSMP/Ranks/GoalEvents/Playtime.java
@@ -0,0 +1,55 @@
+package me.VoltMC.VoltSMP.Ranks.GoalEvents;
+
+import me.VoltMC.VoltSMP.FileManip.PlaytimeData;
+import me.VoltMC.VoltSMP.Main;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+
+public class Playtime implements Listener {
+
+    private static HashMap<Player, Integer> playtimeTracker = new HashMap<>();
+    private static HashMap<Player, Location> afkTracker = new HashMap<>(); // Will add an AFK checker Method later on.
+
+    public static Integer playerPlaytime(Player p) {
+        return playtimeTracker.get(p);
+    }
+
+    public static void editPlayerTime(Player p, Integer time) {
+        if (!playtimeTracker.containsKey(p)) return;
+        playtimeTracker.remove(p);
+        playtimeTracker.put(p, time + 1);
+    }
+
+    public static void TrackPlaytime() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+              //  if (playtimeTracker.isEmpty()) this.cancel();
+                for (Player p : playtimeTracker.keySet()) {
+                    if (p == null) return;
+                    if (playtimeTracker.get(p) == null) return;
+                    int currentTime = playtimeTracker.get(p);
+                    editPlayerTime(p, currentTime);
+                }
+            }
+        }.runTaskTimerAsynchronously(Main.getInstance(), 0L, 20L);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        PlaytimeData.initPlayerTime(p, 0);
+        if (playtimeTracker.containsKey(p)) return;
+        playtimeTracker.put(p, PlaytimeData.getPlayerTime(p));
+    }
+
+
+
+
+}
