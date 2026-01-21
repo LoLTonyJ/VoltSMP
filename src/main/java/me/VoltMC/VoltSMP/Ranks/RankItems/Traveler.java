b1e6a3822c3a510949d13df1d0574cd42540f363
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-21 02:45:28 -0500

    Removed useless lines.

--- src/main/java/me/VoltMC/VoltSMP/Ranks/RankItems/Traveler.java
+++ src/main/java/me/VoltMC/VoltSMP/Ranks/RankItems/Traveler.java
@@ -1,10 +1,12 @@
 package me.VoltMC.VoltSMP.Ranks.RankItems;
 
+import me.VoltMC.VoltSMP.FileManip.RankTracking.MinerTrack;
 import me.VoltMC.VoltSMP.FileManip.RankTracking.TravelerTrack;
 import me.VoltMC.VoltSMP.Main;
 import me.VoltMC.VoltSMP.Util.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -17,26 +19,21 @@ public class Traveler {
 
         // Param @p will be used once config files are running smoothly. Until then, it is a placeholder for future inputs.
 
-        String blocks = Main.getInstance().getConfig().getString("Traveler.Blocks");
-        Integer blockGoal = Integer.parseInt(Main.getInstance().getConfig().getString("Traveler.Blocks"));
+        String blocks = Main.getInstance().getConfig().getString("Miner.Tier_" + MinerTrack.getTier(p) + ".Blocks");
 
         ItemStack item = new ItemStack(Material.COMPASS);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.format("&bTraveler Rank"));
+        meta.setDisplayName(ChatColor.format("&bTraveler Rewards"));
         List<String> lore = new ArrayList<>();
+        if (blocks == null) {
+            lore.add(" ");
+            lore.add(ChatColor.format("&aYou have completed all the goals! Congratulations!"));
+        }
         lore.add(" ");
         lore.add(ChatColor.format("&bObtainable by;"));
         lore.add(ChatColor.format("&7- &aWalking " + blocks + " blocks"));
-        if (TravelerTrack.getPlayerBlock(p) <= blockGoal) {
-            lore.add(" ");
-            lore.add(ChatColor.format("&aYou have traveled " + TravelerTrack.getPlayerBlock(p) + " Blocks"));
-            lore.add(" ");
-        } else {
-            lore.add(" ");
-            lore.add(ChatColor.format("&a&lClick to claim your Reward!"));
-            lore.add(" ");
-        }
         meta.setLore(lore);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
         item.setItemMeta(meta);
         return item;
     }
