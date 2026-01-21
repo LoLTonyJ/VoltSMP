821e1bf0be8b057ab6647793e7b9cd69c6b08b4a
Ghostinq <anthony.hewitson98@outlook.com>
2026-01-21 02:42:34 -0500

    Fixed set player rank, and permission add.

--- src/main/java/me/VoltMC/VoltSMP/Functions/givePlayerRank.java
+++ src/main/java/me/VoltMC/VoltSMP/Functions/givePlayerRank.java
@@ -4,12 +4,21 @@ import me.VoltMC.VoltSMP.Main;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.node.Node;
+import net.luckperms.api.node.types.InheritanceNode;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class givePlayerRank {
+
+    public static void addPermission(Player p, String permission) {
+        UUID pUUID = p.getUniqueId();
+        Main.getInstance().luckAPI().getUserManager().modifyUser(pUUID, user -> {
+            Node permissionNode = Node.builder(permission).build();
+            user.data().add(permissionNode);
+        });
+    }
 
     public static void removePlayerRank(Player p, String rank) {
         UUID pUUID = p.getUniqueId();
@@ -21,14 +30,29 @@ public class givePlayerRank {
         Main.getInstance().luckAPI().getUserManager().saveUser(user);
     }
 
-    public static void setPLayerRank(Player p, String rank) {
+    public static void setPlayerRank(Player p, String rank) {
         UUID pUUID = p.getUniqueId();
-        CompletableFuture<User> userFuture = Main.getInstance().luckAPI().getUserManager().loadUser(pUUID);
-        User user = userFuture.join();
+        LuckPerms lp = Main.getInstance().luckAPI();
 
-        Node pRank = Node.builder("group." + rank).build();
-        user.data().add(pRank);
-        Main.getInstance().luckAPI().getUserManager().saveUser(user);
+        System.out.println("Running set rank");
+
+        if (rank == null || rank.isEmpty()) {
+            System.out.println(rank + " does not exist, please create the group!");
+            return;
+        }
+
+        lp.getUserManager().loadUser(pUUID).thenAccept(user -> {
+            user.data().clear(node -> node instanceof InheritanceNode);
+            user.data().add(InheritanceNode.builder(rank).build());
+            user.data().clear(node ->
+                    node.getKey().equals("primary-group")
+            );
+            user.data().add(
+                    net.luckperms.api.node.types.MetaNode.builder("primary-group", rank).build()
+            );
+
+            lp.getUserManager().saveUser(user);
+        });
     }
 
 
